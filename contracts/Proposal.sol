055c98f0bdfb12ff7bc74c40e6c87a09bd7a61e6
Itshalffull <magicmattg@gmail.com>
2015-12-02 12:07:33 -0800

    Adding endTimes, startTimes, and a function to end the contract

--- contracts/Proposal.sol
+++ contracts/Proposal.sol
@@ -1,23 +1,43 @@
 contract Proposal {
 	address owner;
 	string public name;
+	string public description;
 	mapping (address => bool) public voted;
+	uint public voters;
+	uint public funds;
 	uint public forVotes;
 	uint public againstVotes;
+	uint public startTime;
+	uint public endTime;
 
-	function Proposal(string newName) {
+	function Proposal(string newName, string newDescription) {
 		owner = msg.sender;
 		name = newName;
+		description = newDescription;
 	}
 
 	function vote(bool vote, uint weight, address member) {
 		if(msg.sender != owner) { return; }
-		voted[member] = true;
-		if(vote) {
-			forVotes += weight;
+		if(now >= startTime && now <= endTime){
+			voted[member] = true;
+			voters++;
+			funds += weight ** 2;
+			if(vote) {
+				forVotes += weight;
+				return;
+			}
+			againstVotes += weight;
 			return;
 		}
-		againstVotes += weight;
-		return;
+	}
+	function endVote() {
+	    if (now >= block.timestamp){
+	        uint averagePayment = funds/voters;
+	        for (uint i = 0; i < voters; i++){
+	            function do(){
+	                
+	            }
+	        }
+	    }
 	}
 }
